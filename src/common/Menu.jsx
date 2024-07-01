3ce48e5fa99a9e10e02df1f6a0ec8566230377b2
Lucas Capdevila <lcapdevila60@gmail.com>
2024-07-01 17:21:21 -0300

    se agrega navbar

--- src/common/Menu.jsx
+++ src/common/Menu.jsx
@@ -1,23 +1,25 @@
-import { Navbar } from 'react-bootstrap'
+import { Container, Navbar } from 'react-bootstrap'
 import { Link } from 'react-router-dom'
 
 const Menu = () => {
   return (
     <Navbar bg="dark" expand="lg">
-      <Navbar.Brand>
-        <Link className='nav-link list-unstyled text-white' to='/'>Inicio</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <ul>
-          <li className="nav-item">
-            <Link className='nav-link list-unstyled text-white' to='/lista'>Lista de Transacciones</Link> 
-          </li>
-          <li className="nav-item">
-            <Link className='nav-link list-unstyled text-white' to='/buscador'>Buscador de Transacciones</Link> 
-          </li>
-        </ul>
-      </Navbar.Collapse>
+      <Container>
+        <Navbar.Brand>
+          <Link className='nav-link list-unstyled text-white' to='/'>Inicio</Link>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <ul>
+            <li className="nav-item">
+              <Link className='nav-link list-unstyled text-white' to='/lista'>Lista de Transacciones</Link> 
+            </li>
+            <li className="nav-item">
+              <Link className='nav-link list-unstyled text-white' to='/buscador'>Buscador de Transacciones</Link> 
+            </li>
+          </ul>
+        </Navbar.Collapse>
+      </Container>
     </Navbar>
   )
 }
