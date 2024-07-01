27a4bffce9d60c5a1918939bb4d4c90fde742cf8
Lucas Capdevila <lcapdevila60@gmail.com>
2024-07-01 18:44:49 -0300

    se realiza página de resumen

--- src/pages/Resumen.jsx
+++ src/pages/Resumen.jsx
@@ -1,12 +1,27 @@
+import { useSelector } from "react-redux"
 
 const Resumen = () => {
+  const listaTransacciones = useSelector((state) => state.transacciones)
+
+  let ingresosTotales = 0
+  let gastosTotales = 0
+
+  listaTransacciones.map((transaccion) => {
+    if(transaccion.ingresoOGasto === 'gasto'){
+      gastosTotales += Number(transaccion.monto)
+    } else{
+      ingresosTotales += Number(transaccion.monto)
+      console.log(ingresosTotales);
+    }
+  })
+
   return (
     <div>
       <h2>Resumen de la cuenta</h2>
 
-      <p>Ingresos totales: </p>
-      <p>Gastos totales: </p>
-      <p>Saldo final: </p>
+      <p>Ingresos totales: {ingresosTotales}</p>
+      <p>Gastos totales: {gastosTotales}</p>
+      <p>Saldo final: {ingresosTotales - gastosTotales}</p>
     </div>
   )
 }
